c6eb808fa21d635e79ffeadf390845b743381e66
Michael Miller <icy.arctic.fox@gmail.com>
2019-05-19 13:21:40 -0600

    Update

---
+++ src/opengl/program_property_arb.cr
@@ -0,0 +1,22 @@
+@[Link("gl")]
+lib LibGL
+  enum ProgramPropertyARB : ::UInt32
+    DeleteStatus = 0x8b80
+
+    LinkStatus = 0x8b82
+
+    ValidateStatus = 0x8b83
+
+    InfoLogLength = 0x8b84
+
+    AttachedShaders = 0x8b85
+
+    ActiveUniforms = 0x8b86
+
+    ActiveUniformMaxLength = 0x8b87
+
+    ActiveAttributes = 0x8b89
+
+    ActiveAttributeMaxLength = 0x8b8a
+  end
+end
---
+++ src/opengl/rendering_mode.cr
@@ -0,0 +1,10 @@
+@[Link("gl")]
+lib LibGL
+  enum RenderingMode : ::UInt32
+    Render = 0x1c00
+
+    Feedback = 0x1c01
+
+    Select = 0x1c02
+  end
+end
