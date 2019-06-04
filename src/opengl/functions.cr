1efbdd6c05be13936dbb052a5598d5c0e116e3c7
Michael Miller <icy.arctic.fox@gmail.com>
2019-06-04 13:23:23 -0600

    Update

--- src/opengl/functions.cr
+++ src/opengl/functions.cr
@@ -3049,7 +3049,7 @@ lib LibGL
   ) : ::Void
 
   fun get_integer_i_v = glGetIntegeri_v(
-    target : TypeEnum,
+    target : Enum,
     index : UInt,
     data : ::Pointer(Int)
   ) : ::Void
@@ -3113,7 +3113,7 @@ lib LibGL
 
   fun begin_conditional_render = glBeginConditionalRender(
     id : UInt,
-    mode : TypeEnum
+    mode : ConditionalRenderMode
   ) : ::Void
 
   fun end_conditional_render = glEndConditionalRender : ::Void
@@ -3690,7 +3690,7 @@ lib LibGL
   ) : ::Void
 
   fun get_integer_64i_v = glGetInteger64i_v(
-    target : TypeEnum,
+    target : Enum,
     index : UInt,
     data : ::Pointer(Int64)
   ) : ::Void
@@ -4929,13 +4929,13 @@ lib LibGL
   ) : ::Void
 
   fun get_float_i_v = glGetFloati_v(
-    target : TypeEnum,
+    target : Enum,
     index : UInt,
     data : ::Pointer(Float)
   ) : ::Void
 
   fun get_double_i_v = glGetDoublei_v(
-    target : TypeEnum,
+    target : Enum,
     index : UInt,
     data : ::Pointer(Double)
   ) : ::Void
