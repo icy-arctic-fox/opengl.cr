2bf189f67f19d6218bb1c0d634f468346cff8372
Michael Miller <icy.arctic.fox@gmail.com>
2019-06-23 21:30:12 -0600

    Update

--- src/opengl/sampler_parameter_i.cr
+++ src/opengl/sampler_parameter_i.cr
@@ -1,8 +1,6 @@
 @[Link("gl")]
 lib LibGL
-  enum SamplerParameterName : ::Int32
-    TextureBorderColor = 0x1004
-
+  enum SamplerParameterI : ::Int32
     TextureMagFilter = 0x2800
 
     TextureMinFilter = 0x2801
@@ -12,9 +10,5 @@ lib LibGL
     TextureWrapT = 0x2803
 
     TextureWrapR = 0x8072
-
-    TextureMinLod = 0x813a
-
-    TextureMaxLod = 0x813b
   end
 end
