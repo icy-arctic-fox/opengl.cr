69424631bf066ae64706e5e4e0e02c8680ca121f
Michael Miller <icy.arctic.fox@gmail.com>
2019-06-23 21:29:43 -0600

    Update

---
+++ src/opengl/glsl_type_token.cr
@@ -0,0 +1,14 @@
+@[Link("gl")]
+lib LibGL
+  # OpenGL 4.6 Table 7.3: OpenGL Shading Language type tokens
+
+  enum SLTypeToken : ::Int32
+    Int = 0x1404
+
+    UnsignedInt = 0x1405
+
+    Float = 0x1406
+
+    Double = 0x140a
+  end
+end
