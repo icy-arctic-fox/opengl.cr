6fb2d0b4f4bfcde70f92f7af3345a2948e6156dc
Michael Miller <icy.arctic.fox@gmail.com>
2019-06-23 21:29:57 -0600

    Update

---
+++ src/opengl/clamp_color_mode_arb.cr
@@ -0,0 +1,8 @@
+@[Link("gl")]
+lib LibGL
+  enum ClampColorModeARB : ::Int32
+    False = 0x0
+
+    True = 0x1
+  end
+end
---
+++ src/opengl/sampler_parameter_f.cr
@@ -0,0 +1,10 @@
+@[Link("gl")]
+lib LibGL
+  enum SamplerParameterF : ::Int32
+    TextureBorderColor = 0x1004
+
+    TextureMinLod = 0x813a
+
+    TextureMaxLod = 0x813b
+  end
+end
