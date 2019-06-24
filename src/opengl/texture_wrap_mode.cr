145211957acb4de382e1c7eca2eb36196e375039
Michael Miller <icy.arctic.fox@gmail.com>
2019-06-23 21:34:09 -0600

    Update

--- src/opengl/texture_wrap_mode.cr
+++ src/opengl/texture_wrap_mode.cr
@@ -1,6 +1,8 @@
 @[Link("gl")]
 lib LibGL
   enum TextureWrapMode : ::Int32
+    LinearMipmapLinear = 0x2703
+
     Repeat = 0x2901
 
     Clamp = 0x2900
@@ -8,5 +10,7 @@ lib LibGL
     ClampToEdge = 0x812f
 
     ClampToBorder = 0x812d
+
+    MirroredRepeat = 0x8370
   end
 end
