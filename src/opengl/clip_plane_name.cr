7e1db87055c79fd1eb329a5f68e29abfcc1b2311
Michael Miller <icy.arctic.fox@gmail.com>
2019-07-27 23:16:39 -0600

    Update

--- src/opengl/clip_plane_name.cr
+++ src/opengl/clip_plane_name.cr
@@ -1,4 +1,8 @@
-@[Link("gl")]
+{% if flag?(:darwin) %}
+  @[Link(framework: "OpenGL")]
+{% else %}
+  @[Link("gl")]
+{% end %}
 lib LibGL
   enum ClipPlaneName : ::UInt32
     ClipPlane0 = 0x3000
--- src/opengl/sgi.cr
+++ src/opengl/sgi.cr
@@ -1,4 +1,8 @@
-@[Link("gl")]
+{% if flag?(:darwin) %}
+  @[Link(framework: "OpenGL")]
+{% else %}
+  @[Link("gl")]
+{% end %}
 lib LibGL
   UNSIGNED_BYTE_2_3_3_REV = 33634
 
