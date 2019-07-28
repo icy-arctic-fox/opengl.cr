306587b592e4e5a0c655e75ee847f8e52d69bcb7
Michael Miller <icy.arctic.fox@gmail.com>
2019-07-27 23:20:52 -0600

    Update

--- src/opengl/debug_type.cr
+++ src/opengl/debug_type.cr
@@ -1,4 +1,8 @@
-@[Link("gl")]
+{% if flag?(:darwin) %}
+  @[Link(framework: "OpenGL")]
+{% else %}
+  @[Link("gl")]
+{% end %}
 lib LibGL
   enum DebugType : ::UInt32
     DontCare = 0x1100
--- src/opengl/get_pointerv_p_name.cr
+++ src/opengl/get_pointerv_p_name.cr
@@ -1,4 +1,8 @@
-@[Link("gl")]
+{% if flag?(:darwin) %}
+  @[Link(framework: "OpenGL")]
+{% else %}
+  @[Link("gl")]
+{% end %}
 lib LibGL
   enum GetPointervPName : ::UInt32
     VertexArrayPointer = 0x808e
