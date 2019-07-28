37c6a24ed952c9558dfa5ea65029adc75393c6f6
Michael Miller <icy.arctic.fox@gmail.com>
2019-07-27 22:59:19 -0600

    Update

---
+++ src/opengl/copy_image_sub_data_target.cr
@@ -0,0 +1,12 @@
+@[Link("gl")]
+lib LibGL
+  enum CopyImageSubDataTarget : ::Int32
+    Texture1D = 0xde0
+
+    Texture2D = 0xde1
+
+    Texture3D = 0x806f
+
+    TextureCubeMap = 0x8513
+  end
+end
