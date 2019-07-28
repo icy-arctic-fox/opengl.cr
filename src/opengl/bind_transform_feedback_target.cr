5acdff6b2c868eb29bb8543a35fb82420e334a1c
Michael Miller <icy.arctic.fox@gmail.com>
2019-07-27 23:21:36 -0600

    Update

--- src/opengl/bind_transform_feedback_target.cr
+++ src/opengl/bind_transform_feedback_target.cr
@@ -1,4 +1,8 @@
-@[Link("gl")]
+{% if flag?(:darwin) %}
+  @[Link(framework: "OpenGL")]
+{% else %}
+  @[Link("gl")]
+{% end %}
 lib LibGL
   enum BindTransformFeedbackTarget : ::UInt32
     TransformFeedback = 0x8e22
--- src/opengl/transform_feedback_p_name.cr
+++ src/opengl/transform_feedback_p_name.cr
@@ -1,4 +1,8 @@
-@[Link("gl")]
+{% if flag?(:darwin) %}
+  @[Link(framework: "OpenGL")]
+{% else %}
+  @[Link("gl")]
+{% end %}
 lib LibGL
   enum TransformFeedbackPName : ::UInt32
     TransformFeedbackBufferStart = 0x8c84
