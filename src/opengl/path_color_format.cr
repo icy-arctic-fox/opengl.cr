b62d66f0e41f94402e775f01a35ca83fc65d4cc7
Michael Miller <icy.arctic.fox@gmail.com>
2019-08-25 08:22:21 -0600

    Update

---
+++ src/opengl/path_color_format.cr
@@ -0,0 +1,22 @@
+{% if flag?(:darwin) %}
+  @[Link(framework: "OpenGL")]
+{% else %}
+  @[Link("gl")]
+{% end %}
+lib LibGL
+  enum PathColorFormat : ::Int32
+    None = 0x0
+
+    Alpha = 0x1906
+
+    RGB = 0x1907
+
+    RGBA = 0x1908
+
+    Luminance = 0x1909
+
+    LuminanceAlpha = 0x190a
+
+    Intensity = 0x8049
+  end
+end
