48b8820a085971da4c7bd252d0e80bb0483250c0
Michael Miller <icy.arctic.fox@gmail.com>
2019-10-20 19:24:12 -0600

    Update

---
+++ src/opengl/invalidate_framebuffer_attachment.cr
@@ -0,0 +1,14 @@
+{% if flag?(:darwin) %}
+  @[Link(framework: "OpenGL")]
+{% else %}
+  @[Link("gl")]
+{% end %}
+lib LibGL
+  enum InvalidateFramebufferAttachment : ::Int32
+    Color = 0x1800
+
+    Depth = 0x1801
+
+    Stencil = 0x1802
+  end
+end
