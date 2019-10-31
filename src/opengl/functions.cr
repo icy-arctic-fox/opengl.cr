db5e4fadd8fd28dd916e6de385983fd35eed85f2
Michael Miller <icy.arctic.fox@gmail.com>
2019-10-31 16:00:42 -0600

    Update

--- src/opengl/functions.cr
+++ src/opengl/functions.cr
@@ -1873,7 +1873,7 @@ lib LibGL
   fun get_sync_iv = glGetSynciv(
     sync : Sync,
     pname : SyncParameterName,
-    bufSize : SizeI,
+    count : SizeI,
     length : ::Pointer(SizeI),
     values : ::Pointer(Int)
   ) : ::Void
@@ -2279,7 +2279,7 @@ lib LibGL
     program : UInt,
     shadertype : ShaderType,
     index : UInt,
-    bufsize : SizeI,
+    bufSize : SizeI,
     length : ::Pointer(SizeI),
     name : ::Pointer(Char)
   ) : ::Void
@@ -2288,7 +2288,7 @@ lib LibGL
     program : UInt,
     shadertype : ShaderType,
     index : UInt,
-    bufsize : SizeI,
+    bufSize : SizeI,
     length : ::Pointer(SizeI),
     name : ::Pointer(Char)
   ) : ::Void
@@ -3002,7 +3002,7 @@ lib LibGL
     target : TextureTarget,
     internalformat : InternalFormat,
     pname : InternalFormatPName,
-    bufSize : SizeI,
+    count : SizeI,
     params : ::Pointer(Int)
   ) : ::Void
 
