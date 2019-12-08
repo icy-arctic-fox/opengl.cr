1e167a2c29d99857e7c04fb723cf62bec9e24992
Michael Miller <icy.arctic.fox@gmail.com>
2019-12-08 11:14:20 -0700

    Update

---
+++ src/opengl/combiner_component_usage_nv.cr
@@ -0,0 +1,12 @@
+{% if flag?(:darwin) %}
+  @[Link(framework: "OpenGL")]
+{% else %}
+  @[Link("gl")]
+{% end %}
+lib LibGL
+  enum CombinerComponentUsageNV : ::Int32
+    Alpha = 0x1906
+
+    RGB = 0x1907
+  end
+end
