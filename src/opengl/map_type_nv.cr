79d0d54395753928f6df7f633c756de29675345e
Michael Miller <icy.arctic.fox@gmail.com>
2019-12-08 11:12:44 -0700

    Update

---
+++ src/opengl/map_type_nv.cr
@@ -0,0 +1,12 @@
+{% if flag?(:darwin) %}
+  @[Link(framework: "OpenGL")]
+{% else %}
+  @[Link("gl")]
+{% end %}
+lib LibGL
+  enum MapTypeNV : ::Int32
+    Float = 0x1406
+
+    Double = 0x140a
+  end
+end
