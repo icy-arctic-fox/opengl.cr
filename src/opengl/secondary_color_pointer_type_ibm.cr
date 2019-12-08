53b8554a59190af45df9248869e4f444a5519f3f
Michael Miller <icy.arctic.fox@gmail.com>
2019-12-08 11:08:35 -0700

    Update

---
+++ src/opengl/secondary_color_pointer_type_ibm.cr
@@ -0,0 +1,16 @@
+{% if flag?(:darwin) %}
+  @[Link(framework: "OpenGL")]
+{% else %}
+  @[Link("gl")]
+{% end %}
+lib LibGL
+  enum SecondaryColorPointerTypeIBM : ::Int32
+    Short = 0x1402
+
+    Int = 0x1404
+
+    Float = 0x1406
+
+    Double = 0x140a
+  end
+end
