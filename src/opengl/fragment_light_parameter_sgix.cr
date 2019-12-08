4f3af8477bea45e7d566e61cd2deb7c95bbce8a7
Michael Miller <icy.arctic.fox@gmail.com>
2019-12-08 11:13:53 -0700

    Update

---
+++ src/opengl/fragment_light_parameter_sgix.cr
@@ -0,0 +1,28 @@
+{% if flag?(:darwin) %}
+  @[Link(framework: "OpenGL")]
+{% else %}
+  @[Link("gl")]
+{% end %}
+lib LibGL
+  enum FragmentLightParameterSGIX : ::Int32
+    Ambient = 0x1200
+
+    Diffuse = 0x1201
+
+    Specular = 0x1202
+
+    Position = 0x1203
+
+    SpotDirection = 0x1204
+
+    SpotExponent = 0x1205
+
+    SpotCutoff = 0x1206
+
+    ConstantAttenuation = 0x1207
+
+    LinearAttenuation = 0x1208
+
+    QuadraticAttenuation = 0x1209
+  end
+end
