88d60d4ba81bca6a1b0a80039b835ce0654f0c8c
Michael Miller <icy.arctic.fox@gmail.com>
2019-12-08 11:12:16 -0700

    Update

---
+++ src/opengl/element_pointer_type_ati.cr
@@ -0,0 +1,14 @@
+{% if flag?(:darwin) %}
+  @[Link(framework: "OpenGL")]
+{% else %}
+  @[Link("gl")]
+{% end %}
+lib LibGL
+  enum ElementPointerTypeATI : ::Int32
+    UnsignedByte = 0x1401
+
+    UnsignedShort = 0x1403
+
+    UnsignedInt = 0x1405
+  end
+end
