fb8ae814b8281b375bad20a13c68b0b904299270
Michael Miller <icy.arctic.fox@gmail.com>
2019-12-08 11:10:25 -0700

    Update

---
+++ src/opengl/weight_pointer_type_arb.cr
@@ -0,0 +1,24 @@
+{% if flag?(:darwin) %}
+  @[Link(framework: "OpenGL")]
+{% else %}
+  @[Link("gl")]
+{% end %}
+lib LibGL
+  enum WeightPointerTypeARB : ::Int32
+    Byte = 0x1400
+
+    UnsignedByte = 0x1401
+
+    Short = 0x1402
+
+    UnsignedShort = 0x1403
+
+    Int = 0x1404
+
+    UnsignedInt = 0x1405
+
+    Float = 0x1406
+
+    Double = 0x140a
+  end
+end
