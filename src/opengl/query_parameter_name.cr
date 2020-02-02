f6fd5671eff6f177f9cfc873ac66047ca7f9c67a
Michael Miller <icy.arctic.fox@gmail.com>
2020-02-02 11:07:27 -0700

    Update

--- src/opengl/query_parameter_name.cr
+++ src/opengl/query_parameter_name.cr
@@ -4,6 +4,8 @@
   @[Link("gl")]
 {% end %}
 lib LibGL
+  # Deprecated, use the group attributes instead.
+
   enum QueryParameterName : ::UInt32
     CurrentQuery = 0x8865
   end
--- src/opengl/texture_wrap_mode.cr
+++ src/opengl/texture_wrap_mode.cr
@@ -4,6 +4,8 @@
   @[Link("gl")]
 {% end %}
 lib LibGL
+  # Deprecated, use the group attributes instead.
+
   enum TextureWrapMode : ::Int32
     LinearMipmapLinear = 0x2703
 
