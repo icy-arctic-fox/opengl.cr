0a96b073cb4064483b46c559695c729018a93098
Michael Miller <icy.arctic.fox@gmail.com>
2020-02-07 00:02:03 -0700

    Update

--- src/opengl/program_stage_p_name.cr
+++ src/opengl/program_stage_p_name.cr
@@ -4,7 +4,7 @@
   @[Link("gl")]
 {% end %}
 lib LibGL
-  # Deprecated, use the group attributes instead.
+  # For Pat Brown 2005/10/13
 
   enum ProgramStagePName : ::UInt32
     ActiveSubroutines = 0x8de5
