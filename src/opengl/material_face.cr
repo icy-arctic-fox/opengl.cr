1ee656cfca33c4f15e8a9c359412607422e79cde
Michael Miller <icy.arctic.fox@gmail.com>
2020-03-15 22:28:49 -0600

    Update

---
+++ src/opengl/material_face.cr
@@ -0,0 +1,16 @@
+{% if flag?(:darwin) %}
+  @[Link(framework: "OpenGL")]
+{% else %}
+  @[Link("gl")]
+{% end %}
+lib LibGL
+  # Mostly OpenGL 1.0/1.1 enum assignments. Unused ranges should generally remain unused.
+
+  enum MaterialFace : ::Int32
+    Front = 0x404
+
+    Back = 0x405
+
+    FrontAndBack = 0x408
+  end
+end
