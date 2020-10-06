e2b8379f770b8975f7670f604f0e98ca26876b34
Michael Miller <icy.arctic.fox@gmail.com>
2020-10-06 10:14:25 -0600

    Update

---
+++ src/opengl/sync_behavior_flags.cr
@@ -0,0 +1,12 @@
+{% if flag?(:darwin) %}
+  @[Link(framework: "OpenGL")]
+{% else %}
+  @[Link("gl")]
+{% end %}
+lib LibGL
+  # Tokens whose numeric value is intrinsically meaningful
+
+  enum SyncBehaviorFlagS : ::Int32
+    None = 0x0
+  end
+end
