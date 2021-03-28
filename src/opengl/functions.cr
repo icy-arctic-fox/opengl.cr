69119097fae91a5f2d4a42e52d7a2cee5b81af69
Michael Miller <icy.arctic.fox@gmail.com>
2021-03-28 10:30:57 -0600

    Update

--- src/opengl/functions.cr
+++ src/opengl/functions.cr
@@ -3550,7 +3550,7 @@ lib LibGL
 
   fun tex_buffer = glTexBuffer(
     target : TextureTarget,
-    internalformat : InternalFormat,
+    internalformat : SizedInternalFormat,
     buffer : UInt
   ) : ::Void
 
@@ -5003,14 +5003,14 @@ lib LibGL
   fun tex_storage_1d = glTexStorage1D(
     target : TextureTarget,
     levels : SizeI,
-    internalformat : InternalFormat,
+    internalformat : SizedInternalFormat,
     width : SizeI
   ) : ::Void
 
   fun tex_storage_2d = glTexStorage2D(
     target : TextureTarget,
     levels : SizeI,
-    internalformat : InternalFormat,
+    internalformat : SizedInternalFormat,
     width : SizeI,
     height : SizeI
   ) : ::Void
@@ -5018,7 +5018,7 @@ lib LibGL
   fun tex_storage_3d = glTexStorage3D(
     target : TextureTarget,
     levels : SizeI,
-    internalformat : InternalFormat,
+    internalformat : SizedInternalFormat,
     width : SizeI,
     height : SizeI,
     depth : SizeI
@@ -5039,7 +5039,7 @@ lib LibGL
 
   fun clear_buffer_data = glClearBufferData(
     target : BufferStorageTarget,
-    internalformat : InternalFormat,
+    internalformat : SizedInternalFormat,
     format : PixelFormat,
     type : PixelType,
     data : ::Pointer(Void)
@@ -5047,7 +5047,7 @@ lib LibGL
 
   fun clear_buffer_sub_data = glClearBufferSubData(
     target : BufferTargetARB,
-    internalformat : InternalFormat,
+    internalformat : SizedInternalFormat,
     offset : IntPtr,
     size : SizeIPtr,
     format : PixelFormat,
@@ -5213,7 +5213,7 @@ lib LibGL
 
   fun tex_buffer_range = glTexBufferRange(
     target : TextureTarget,
-    internalformat : InternalFormat,
+    internalformat : SizedInternalFormat,
     buffer : UInt,
     offset : IntPtr,
     size : SizeIPtr
@@ -5222,7 +5222,7 @@ lib LibGL
   fun tex_storage_2d_multisample = glTexStorage2DMultisample(
     target : TextureTarget,
     samples : SizeI,
-    internalformat : InternalFormat,
+    internalformat : SizedInternalFormat,
     width : SizeI,
     height : SizeI,
     fixedsamplelocations : Boolean
@@ -5231,7 +5231,7 @@ lib LibGL
   fun tex_storage_3d_multisample = glTexStorage3DMultisample(
     target : TextureTarget,
     samples : SizeI,
-    internalformat : InternalFormat,
+    internalformat : SizedInternalFormat,
     width : SizeI,
     height : SizeI,
     depth : SizeI,
@@ -5242,7 +5242,7 @@ lib LibGL
     texture : UInt,
     target : TextureTarget,
     origtexture : UInt,
-    internalformat : InternalFormat,
+    internalformat : SizedInternalFormat,
     minlevel : UInt,
     numlevels : UInt,
     minlayer : UInt,
