18d4eca0d78f3e71329fdd850ba39efda0346750
Michael Miller <icy.arctic.fox@gmail.com>
2021-09-04 12:20:01 -0600

    Update

---
+++ src/opengl/translations.cr
@@ -0,0 +1,641 @@
+module OpenGL
+  module Translations
+    extend self
+
+    # Original OpenGL function name: glActiveTexture
+    @[AlwaysInline]
+    def active_texture
+      "glActiveTexture"
+    end
+
+    # Original OpenGL function name: glAlphaFuncx
+    @[AlwaysInline]
+    def alpha_funcx
+      "glAlphaFuncx"
+    end
+
+    # Original OpenGL function name: glBindBuffer
+    @[AlwaysInline]
+    def bind_buffer
+      "glBindBuffer"
+    end
+
+    # Original OpenGL function name: glBindTexture
+    @[AlwaysInline]
+    def bind_texture
+      "glBindTexture"
+    end
+
+    # Original OpenGL function name: glBlendFunc
+    @[AlwaysInline]
+    def blend_func
+      "glBlendFunc"
+    end
+
+    # Original OpenGL function name: glBufferData
+    @[AlwaysInline]
+    def buffer_data
+      "glBufferData"
+    end
+
+    # Original OpenGL function name: glBufferSubData
+    @[AlwaysInline]
+    def buffer_sub_data
+      "glBufferSubData"
+    end
+
+    # Original OpenGL function name: glClear
+    @[AlwaysInline]
+    def clear
+      "glClear"
+    end
+
+    # Original OpenGL function name: glClearColorx
+    @[AlwaysInline]
+    def clear_colorx
+      "glClearColorx"
+    end
+
+    # Original OpenGL function name: glClearDepthx
+    @[AlwaysInline]
+    def clear_depth_x
+      "glClearDepthx"
+    end
+
+    # Original OpenGL function name: glClearStencil
+    @[AlwaysInline]
+    def clear_stencil
+      "glClearStencil"
+    end
+
+    # Original OpenGL function name: glClientActiveTexture
+    @[AlwaysInline]
+    def client_active_texture
+      "glClientActiveTexture"
+    end
+
+    # Original OpenGL function name: glClipPlanex
+    @[AlwaysInline]
+    def clip_planex
+      "glClipPlanex"
+    end
+
+    # Original OpenGL function name: glColor4ub
+    @[AlwaysInline]
+    def color_4ub
+      "glColor4ub"
+    end
+
+    # Original OpenGL function name: glColor4x
+    @[AlwaysInline]
+    def color_4x
+      "glColor4x"
+    end
+
+    # Original OpenGL function name: glColorMask
+    @[AlwaysInline]
+    def color_mask
+      "glColorMask"
+    end
+
+    # Original OpenGL function name: glColorPointer
+    @[AlwaysInline]
+    def color_pointer
+      "glColorPointer"
+    end
+
+    # Original OpenGL function name: glCompressedTexImage2D
+    @[AlwaysInline]
+    def compressed_tex_image_2d
+      "glCompressedTexImage2D"
+    end
+
+    # Original OpenGL function name: glCompressedTexSubImage2D
+    @[AlwaysInline]
+    def compressed_tex_sub_image_2d
+      "glCompressedTexSubImage2D"
+    end
+
+    # Original OpenGL function name: glCopyTexImage2D
+    @[AlwaysInline]
+    def copy_tex_image_2d
+      "glCopyTexImage2D"
+    end
+
+    # Original OpenGL function name: glCopyTexSubImage2D
+    @[AlwaysInline]
+    def copy_tex_sub_image_2d
+      "glCopyTexSubImage2D"
+    end
+
+    # Original OpenGL function name: glCullFace
+    @[AlwaysInline]
+    def cull_face
+      "glCullFace"
+    end
+
+    # Original OpenGL function name: glDeleteBuffers
+    @[AlwaysInline]
+    def delete_buffers
+      "glDeleteBuffers"
+    end
+
+    # Original OpenGL function name: glDeleteTextures
+    @[AlwaysInline]
+    def delete_textures
+      "glDeleteTextures"
+    end
+
+    # Original OpenGL function name: glDepthFunc
+    @[AlwaysInline]
+    def depth_func
+      "glDepthFunc"
+    end
+
+    # Original OpenGL function name: glDepthMask
+    @[AlwaysInline]
+    def depth_mask
+      "glDepthMask"
+    end
+
+    # Original OpenGL function name: glDepthRangex
+    @[AlwaysInline]
+    def depth_rangex
+      "glDepthRangex"
+    end
+
+    # Original OpenGL function name: glDisable
+    @[AlwaysInline]
+    def disable
+      "glDisable"
+    end
+
+    # Original OpenGL function name: glDisableClientState
+    @[AlwaysInline]
+    def disable_client_state
+      "glDisableClientState"
+    end
+
+    # Original OpenGL function name: glDrawArrays
+    @[AlwaysInline]
+    def draw_arrays
+      "glDrawArrays"
+    end
+
+    # Original OpenGL function name: glDrawElements
+    @[AlwaysInline]
+    def draw_elements
+      "glDrawElements"
+    end
+
+    # Original OpenGL function name: glEnable
+    @[AlwaysInline]
+    def enable
+      "glEnable"
+    end
+
+    # Original OpenGL function name: glEnableClientState
+    @[AlwaysInline]
+    def enable_client_state
+      "glEnableClientState"
+    end
+
+    # Original OpenGL function name: glFinish
+    @[AlwaysInline]
+    def finish
+      "glFinish"
+    end
+
+    # Original OpenGL function name: glFlush
+    @[AlwaysInline]
+    def flush
+      "glFlush"
+    end
+
+    # Original OpenGL function name: glFogx
+    @[AlwaysInline]
+    def fog_x
+      "glFogx"
+    end
+
+    # Original OpenGL function name: glFogxv
+    @[AlwaysInline]
+    def fog_xv
+      "glFogxv"
+    end
+
+    # Original OpenGL function name: glFrontFace
+    @[AlwaysInline]
+    def front_face
+      "glFrontFace"
+    end
+
+    # Original OpenGL function name: glFrustumx
+    @[AlwaysInline]
+    def frustumx
+      "glFrustumx"
+    end
+
+    # Original OpenGL function name: glGetBooleanv
+    @[AlwaysInline]
+    def get_boolean_v
+      "glGetBooleanv"
+    end
+
+    # Original OpenGL function name: glGetBufferParameteriv
+    @[AlwaysInline]
+    def get_buffer_parameter_iv
+      "glGetBufferParameteriv"
+    end
+
+    # Original OpenGL function name: glGetClipPlanex
+    @[AlwaysInline]
+    def get_clip_plane_x
+      "glGetClipPlanex"
+    end
+
+    # Original OpenGL function name: glGenBuffers
+    @[AlwaysInline]
+    def gen_buffers
+      "glGenBuffers"
+    end
+
+    # Original OpenGL function name: glGenTextures
+    @[AlwaysInline]
+    def gen_textures
+      "glGenTextures"
+    end
+
+    # Original OpenGL function name: glGetError
+    @[AlwaysInline]
+    def get_error
+      "glGetError"
+    end
+
+    # Original OpenGL function name: glGetFixedv
+    @[AlwaysInline]
+    def get_fixedv
+      "glGetFixedv"
+    end
+
+    # Original OpenGL function name: glGetIntegerv
+    @[AlwaysInline]
+    def get_integer_v
+      "glGetIntegerv"
+    end
+
+    # Original OpenGL function name: glGetLightxv
+    @[AlwaysInline]
+    def get_light_xv
+      "glGetLightxv"
+    end
+
+    # Original OpenGL function name: glGetMaterialxv
+    @[AlwaysInline]
+    def get_material_xv
+      "glGetMaterialxv"
+    end
+
+    # Original OpenGL function name: glGetPointerv
+    @[AlwaysInline]
+    def get_pointer_v
+      "glGetPointerv"
+    end
+
+    # Original OpenGL function name: glGetString
+    @[AlwaysInline]
+    def get_string
+      "glGetString"
+    end
+
+    # Original OpenGL function name: glGetTexEnviv
+    @[AlwaysInline]
+    def get_tex_env_iv
+      "glGetTexEnviv"
+    end
+
+    # Original OpenGL function name: glGetTexEnvxv
+    @[AlwaysInline]
+    def get_tex_env_xv
+      "glGetTexEnvxv"
+    end
+
+    # Original OpenGL function name: glGetTexParameteriv
+    @[AlwaysInline]
+    def get_tex_parameter_iv
+      "glGetTexParameteriv"
+    end
+
+    # Original OpenGL function name: glGetTexParameterxv
+    @[AlwaysInline]
+    def get_tex_parameter_xv
+      "glGetTexParameterxv"
+    end
+
+    # Original OpenGL function name: glHint
+    @[AlwaysInline]
+    def hint
+      "glHint"
+    end
+
+    # Original OpenGL function name: glIsBuffer
+    @[AlwaysInline]
+    def is_buffer
+      "glIsBuffer"
+    end
+
+    # Original OpenGL function name: glIsEnabled
+    @[AlwaysInline]
+    def is_enabled
+      "glIsEnabled"
+    end
+
+    # Original OpenGL function name: glIsTexture
+    @[AlwaysInline]
+    def is_texture
+      "glIsTexture"
+    end
+
+    # Original OpenGL function name: glLightModelx
+    @[AlwaysInline]
+    def light_model_x
+      "glLightModelx"
+    end
+
+    # Original OpenGL function name: glLightModelxv
+    @[AlwaysInline]
+    def light_model_xv
+      "glLightModelxv"
+    end
+
+    # Original OpenGL function name: glLightx
+    @[AlwaysInline]
+    def light_x
+      "glLightx"
+    end
+
+    # Original OpenGL function name: glLightxv
+    @[AlwaysInline]
+    def light_xv
+      "glLightxv"
+    end
+
+    # Original OpenGL function name: glLineWidthx
+    @[AlwaysInline]
+    def line_widthx
+      "glLineWidthx"
+    end
+
+    # Original OpenGL function name: glLoadIdentity
+    @[AlwaysInline]
+    def load_identity
+      "glLoadIdentity"
+    end
+
+    # Original OpenGL function name: glLoadMatrixx
+    @[AlwaysInline]
+    def load_matrix_x
+      "glLoadMatrixx"
+    end
+
+    # Original OpenGL function name: glLogicOp
+    @[AlwaysInline]
+    def logic_op
+      "glLogicOp"
+    end
+
+    # Original OpenGL function name: glMaterialx
+    @[AlwaysInline]
+    def material_x
+      "glMaterialx"
+    end
+
+    # Original OpenGL function name: glMaterialxv
+    @[AlwaysInline]
+    def material_xv
+      "glMaterialxv"
+    end
+
+    # Original OpenGL function name: glMatrixMode
+    @[AlwaysInline]
+    def matrix_mode
+      "glMatrixMode"
+    end
+
+    # Original OpenGL function name: glMultMatrixx
+    @[AlwaysInline]
+    def mult_matrix_x
+      "glMultMatrixx"
+    end
+
+    # Original OpenGL function name: glMultiTexCoord4x
+    @[AlwaysInline]
+    def multi_tex_coord_4x
+      "glMultiTexCoord4x"
+    end
+
+    # Original OpenGL function name: glNormal3x
+    @[AlwaysInline]
+    def normal_3x
+      "glNormal3x"
+    end
+
+    # Original OpenGL function name: glNormalPointer
+    @[AlwaysInline]
+    def normal_pointer
+      "glNormalPointer"
+    end
+
+    # Original OpenGL function name: glOrthox
+    @[AlwaysInline]
+    def orthox
+      "glOrthox"
+    end
+
+    # Original OpenGL function name: glPixelStorei
+    @[AlwaysInline]
+    def pixel_store_i
+      "glPixelStorei"
+    end
+
+    # Original OpenGL function name: glPointParameterx
+    @[AlwaysInline]
+    def point_parameter_x
+      "glPointParameterx"
+    end
+
+    # Original OpenGL function name: glPointParameterxv
+    @[AlwaysInline]
+    def point_parameter_xv
+      "glPointParameterxv"
+    end
+
+    # Original OpenGL function name: glPointSizex
+    @[AlwaysInline]
+    def point_sizex
+      "glPointSizex"
+    end
+
+    # Original OpenGL function name: glPolygonOffsetx
+    @[AlwaysInline]
+    def polygon_offsetx
+      "glPolygonOffsetx"
+    end
+
+    # Original OpenGL function name: glPopMatrix
+    @[AlwaysInline]
+    def pop_matrix
+      "glPopMatrix"
+    end
+
+    # Original OpenGL function name: glPushMatrix
+    @[AlwaysInline]
+    def push_matrix
+      "glPushMatrix"
+    end
+
+    # Original OpenGL function name: glReadPixels
+    @[AlwaysInline]
+    def read_pixels
+      "glReadPixels"
+    end
+
+    # Original OpenGL function name: glRotatex
+    @[AlwaysInline]
+    def rotate_x
+      "glRotatex"
+    end
+
+    # Original OpenGL function name: glSampleCoverage
+    @[AlwaysInline]
+    def sample_coverage
+      "glSampleCoverage"
+    end
+
+    # Original OpenGL function name: glSampleCoveragex
+    @[AlwaysInline]
+    def sample_coveragex
+      "glSampleCoveragex"
+    end
+
+    # Original OpenGL function name: glScalex
+    @[AlwaysInline]
+    def scale_x
+      "glScalex"
+    end
+
+    # Original OpenGL function name: glScissor
+    @[AlwaysInline]
+    def scissor
+      "glScissor"
+    end
+
+    # Original OpenGL function name: glShadeModel
+    @[AlwaysInline]
+    def shade_model
+      "glShadeModel"
+    end
+
+    # Original OpenGL function name: glStencilFunc
+    @[AlwaysInline]
+    def stencil_func
+      "glStencilFunc"
+    end
+
+    # Original OpenGL function name: glStencilMask
+    @[AlwaysInline]
+    def stencil_mask
+      "glStencilMask"
+    end
+
+    # Original OpenGL function name: glStencilOp
+    @[AlwaysInline]
+    def stencil_op
+      "glStencilOp"
+    end
+
+    # Original OpenGL function name: glTexCoordPointer
+    @[AlwaysInline]
+    def tex_coord_pointer
+      "glTexCoordPointer"
+    end
+
+    # Original OpenGL function name: glTexEnvi
+    @[AlwaysInline]
+    def tex_env_i
+      "glTexEnvi"
+    end
+
+    # Original OpenGL function name: glTexEnvx
+    @[AlwaysInline]
+    def tex_env_x
+      "glTexEnvx"
+    end
+
+    # Original OpenGL function name: glTexEnviv
+    @[AlwaysInline]
+    def tex_env_iv
+      "glTexEnviv"
+    end
+
+    # Original OpenGL function name: glTexEnvxv
+    @[AlwaysInline]
+    def tex_env_xv
+      "glTexEnvxv"
+    end
+
+    # Original OpenGL function name: glTexImage2D
+    @[AlwaysInline]
+    def tex_image_2d
+      "glTexImage2D"
+    end
+
+    # Original OpenGL function name: glTexParameteri
+    @[AlwaysInline]
+    def tex_parameter_i
+      "glTexParameteri"
+    end
+
+    # Original OpenGL function name: glTexParameterx
+    @[AlwaysInline]
+    def tex_parameter_x
+      "glTexParameterx"
+    end
+
+    # Original OpenGL function name: glTexParameteriv
+    @[AlwaysInline]
+    def tex_parameter_iv
+      "glTexParameteriv"
+    end
+
+    # Original OpenGL function name: glTexParameterxv
+    @[AlwaysInline]
+    def tex_parameter_xv
+      "glTexParameterxv"
+    end
+
+    # Original OpenGL function name: glTexSubImage2D
+    @[AlwaysInline]
+    def tex_sub_image_2d
+      "glTexSubImage2D"
+    end
+
+    # Original OpenGL function name: glTranslatex
+    @[AlwaysInline]
+    def translate_x
+      "glTranslatex"
+    end
+
+    # Original OpenGL function name: glVertexPointer
+    @[AlwaysInline]
+    def vertex_pointer
+      "glVertexPointer"
+    end
+
+    # Original OpenGL function name: glViewport
+    @[AlwaysInline]
+    def viewport
+      "glViewport"
+    end
+  end
+end
