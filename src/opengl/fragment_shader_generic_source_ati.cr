902614724a442d4db977371c05e9338404fc2a2e
Michael Miller <icy.arctic.fox@gmail.com>
2022-10-10 17:18:45 -0600

    Update

---
+++ src/opengl/fragment_shader_generic_source_ati.cr
@@ -0,0 +1,16 @@
+{% if flag?(:darwin) %}
+  @[Link(framework: "OpenGL")]
+{% else %}
+  @[Link("gl")]
+{% end %}
+lib LibGL
+  # Tokens whose numeric value is intrinsically meaningful
+
+  enum FragmentShaderGenericSourceATI : ::Int32
+    Zero = 0x0
+
+    One = 0x1
+
+    PrimaryColor = 0x8577
+  end
+end
--- src/opengl/point_parameter_name_arb.cr
+++ src/opengl/point_parameter_name_arb.cr
@@ -6,5 +6,11 @@
 lib LibGL
   enum PointParameterNameARB : ::UInt32
     PointFadeThresholdSize = 0x8128
+
+    PointSizeMin = 0x8126
+
+    PointSizeMax = 0x8127
+
+    PointDistanceAttenuation = 0x8129
   end
 end
