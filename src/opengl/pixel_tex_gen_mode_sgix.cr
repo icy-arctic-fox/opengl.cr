568e09e61f69e8a393a75294daf87eeac393d1ca
Michael Miller <icy.arctic.fox@gmail.com>
2022-10-10 17:22:25 -0600

    Update

--- src/opengl/pixel_tex_gen_mode_sgix.cr
+++ src/opengl/pixel_tex_gen_mode_sgix.cr
@@ -6,11 +6,13 @@
 lib LibGL
   # Mostly OpenGL 1.0/1.1 enum assignments. Unused ranges should generally remain unused.
 
-  enum ColorMaterialFace : ::Int32
-    Front = 0x404
+  enum PixelTexGenModeSGIX : ::Int32
+    Alpha = 0x1906
 
-    Back = 0x405
+    RGB = 0x1907
 
-    FrontAndBack = 0x408
+    RGBA = 0x1908
+
+    None = 0x0
   end
 end
