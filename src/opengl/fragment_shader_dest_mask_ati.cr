804f742fa7a44cf87c360e67bebfd76028183dfe
Michael Miller <icy.arctic.fox@gmail.com>
2022-10-10 17:18:04 -0600

    Update

--- src/opengl/fragment_shader_dest_mask_ati.cr
+++ src/opengl/fragment_shader_dest_mask_ati.cr
@@ -6,11 +6,7 @@
 lib LibGL
   # Tokens whose numeric value is intrinsically meaningful
 
-  enum PixelTexGenMode : ::Int32
+  enum FragmentShaderDestMaskATI : ::Int32
     None = 0x0
-
-    RGB = 0x1907
-
-    RGBA = 0x1908
   end
 end
