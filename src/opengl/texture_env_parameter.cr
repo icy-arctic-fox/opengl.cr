667d625370da814965a6e85f607054c93924da5d
Michael Miller <icy.arctic.fox@gmail.com>
2023-01-01 10:25:48 -0700

    Update

--- src/opengl/texture_env_parameter.cr
+++ src/opengl/texture_env_parameter.cr
@@ -7,6 +7,8 @@ lib LibGL
   # Mostly OpenGL 1.0/1.1 enum assignments. Unused ranges should generally remain unused.
 
   enum TextureEnvParameter : ::UInt32
+    AlphaScale = 0xd1c
+
     TextureEnvMode = 0x2200
 
     TextureEnvColor = 0x2201
