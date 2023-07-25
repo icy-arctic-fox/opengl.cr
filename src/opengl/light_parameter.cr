f32911b5bb7174ca521c157ab2ca961f96480d65
Michael Miller <icy.arctic.fox@gmail.com>
2023-07-25 16:13:34 -0600

    Update

--- src/opengl/light_parameter.cr
+++ src/opengl/light_parameter.cr
@@ -7,6 +7,12 @@ lib LibGL
   # Mostly OpenGL 1.0/1.1 enum assignments. Unused ranges should generally remain unused.
 
   enum LightParameter : ::UInt32
+    Ambient = 0x1200
+
+    Diffuse = 0x1201
+
+    Specular = 0x1202
+
     Position = 0x1203
 
     SpotDirection = 0x1204
