11e9955404d7787cbb476729797164efb45eceb6
Michael Miller <icy.arctic.fox@gmail.com>
2024-11-30 22:32:41 -0700

    Update

--- src/opengl/texture_wrap_mode.cr
+++ src/opengl/texture_wrap_mode.cr
@@ -7,8 +7,6 @@ lib LibGL
   # Mostly OpenGL 1.0/1.1 enum assignments. Unused ranges should generally remain unused.
 
   enum TextureWrapMode : ::Int32
-    LinearMipmapLinear = 0x2703
-
     Repeat = 0x2901
 
     Clamp = 0x2900
